d73ce711c011bdba2aaa4fb214fce896b547f372
Christian Heussy <heussy@gmail.com>
2023-11-26 12:48:10 -0800

    Updated to use law of cosinses and quadratic formula to determine firing solution.

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,116 @@
 use oort_api::prelude::*;
-
 const UPPER_BOUND: f64 = (2.0 * PI) - 0.2;
 const TWO_RAD: f64 = 2.0 * PI;
 
-const BULLET_SPEED: f64 = 1000.0; // m/s
-
 #[derive(Default)]
-pub struct Ship {
-    target: Option<TargetData>,
-}
-
-struct TargetData {
-    init_pos: Vec2,
-    velocity: Vec2,
-}
-
-impl TargetData {
-    /// Given time in seconds calculate the targets position.
-    /// Assume constant velocity.
-    fn get_position(&self, time: f64) -> Vec2 {
-        self.init_pos + time * self.velocity
-    }
-}
+pub struct Ship {}
 
 impl Ship {
+    /// Constant bullet speed m/s
+    const BULLET_SPEED: f64 = 1000.0;
+
     pub fn new() -> Ship {
-        Ship { target: None }
+        Ship {}
     }
 
-    pub fn tick(&mut self) {
-        if self.target.is_none() {
-            self.target = Some(TargetData {
-                init_pos: target(),
-                velocity: target_velocity(),
-            });
-        }
+    fn stable_heading(&self) -> f64 {
+        let raw_heading = heading();
 
-        let target_data = self.target.as_ref().unwrap();
-
-        let target_vector = target_data.get_position(current_time()) - position();
-
-        let target_angle = target_vector.angle();
-        debug!("Target vector: {:?}", target_vector);
-        debug!("Target angle: {:?}", target_angle);
-
-        let stable_heading = match heading() {
+        let filtered = match raw_heading {
             0.0..=0.2 => 0.0,
             UPPER_BOUND..=TWO_RAD => 0.0,
             val => val,
         };
-        debug!("heading: {}", stable_heading);
 
-        let steering_command = (target_angle - stable_heading);
+        debug!("Filtered heading: {}", filtered);
+        filtered
+    }
+
+    // TODO: steer negative
+    fn steer_to(&self, target_angle: f64) {
+        let steering_command = target_angle - self.stable_heading();
         debug!("Steer: {}", steering_command);
-
         turn(steering_command);
+    }
 
-        draw_line(
-            position(),
-            target_data.get_position(current_time()),
-            0x00ff00,
-        );
+    fn firing_solution(
+        &self,
+        position_bullet: Vec2,
+        position_target: Vec2,
+        velocity_target: Vec2,
+    ) -> Option<Vec2> {
+        // Subtract the target's position from the bullet's position
+        let position_delta = position_bullet - position_target;
 
-        // let dp = target() - position();
-        // debug!("distance to target: {}", dp.length());
-        // debug!("time to target: {}", dp.length() / BULLET_SPEED);
+        // Determine the distance between the target and the bullet
+        let d = position_delta.length();
 
-        // turn(1.0);
-        fire(0);
+        // Determine the magnitude of the target's velocity vector (speed_target)
+        let speed_target = velocity_target.length();
+
+        // Use the law of cosines to obtain quadratic
+        let a = Self::BULLET_SPEED.powf(2.0) - speed_target.powf(2.0);
+        let b = 2.0 * position_delta.dot(position_target);
+        let c = (d * -1.0).powf(2.0);
+
+        // Determine if the quadratic has a solution
+        let discriminant = b.powf(2.0) - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        // Solve quadratic to determine intercept time
+        let t1 = (-b + discriminant.sqrt()) / (2.0 * a);
+        let t2 = (-b - discriminant.sqrt()) / (2.0 * a);
+
+        // Determine valid t
+        let t = {
+            if t1 < 0.0 && t2 < 0.0 {
+                return None;
+            } else if t1 < 0.0 {
+                t2
+            } else if t2 < 0.0 {
+                t1
+            } else {
+                t1.min(t2)
+            }
+        };
+
+        // Now that t is known determine interception point
+        let intercept_point = position_target + t * velocity_target;
+
+        // Calculate velocity vector
+        let velocity_vector = (intercept_point - position_bullet) / t;
+
+        Some(velocity_vector)
+    }
+
+    pub fn tick(&mut self) {
+        debug!("Pb: {}", position());
+        debug!("Pt: {}", target());
+        debug!("Vt: {}", target_velocity());
+        if let Some(solution) = self.firing_solution(position(), target(), target_velocity()) {
+            self.steer_to(solution.angle());
+
+            // TODO: only fire if error within threshold
+            fire(0);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_firing_solution() {
+        let ship = Ship::new();
+        let pb = Vec2::new(0., 0.);
+        let pt = Vec2::new(-2435., 1112.);
+        let vt = Vec2::new(117., -382.);
+        let solution = ship.firing_solution(pb, pt, vt);
+        let angle = solution.unwrap().angle();
+        println!("faldskjdfs")
     }
 }
